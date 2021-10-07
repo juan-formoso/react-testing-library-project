0e4fef1a9dcfeb7bd2349f934a2dc59edf32eedc
Juan Formoso <trajano.juan@gmail.com>
2021-10-07 15:17:14 -0400

    Finalizando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa se FavoritePokemons:', () => {
+  test('Mostra o texto "No favorite pokemon found" se não tiver favoritos', () => {
+    render(<FavoritePokemons />);
+    const noFavMensage = screen.getByText('No favorite pokemon found');
+    expect(noFavMensage).toBeInTheDocument();
+  });
+
+  test('Se os pokémons favoritos aparecem', () => {
+    render(<Router history={ createMemoryHistory() }><App /></Router>);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetailsLink);
+    const favCheck = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favCheck);
+    const favLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favLink);
+    const favPokemon = screen.getByText(/Average weight:/);
+    expect(favPokemon).toBeInTheDocument();
+  });
+});
